09ee00fbddae6440f2c7904b5ce2f70ef05196ea
JeremyGe07 <13361065246@163.com>
2026-06-01 09:06:47 +0800

    [Config] Make A800 revalidation configs portable

--- tests/unit_tests/runner/test_auto_tuner_qwen3_revalidation_config.py
+++ tests/unit_tests/runner/test_auto_tuner_qwen3_revalidation_config.py
@@ -20,14 +20,20 @@ def test_qwen3_14b_a800_autotune_uses_local_runner_and_assets():
     assert cfg["experiment"]["runner"]["nnodes"] == 1
     assert cfg["experiment"]["runner"]["nproc_per_node"] == 4
     assert cfg["experiment"]["envs"]["CUDA_VISIBLE_DEVICES"] == "0,1,2,3"
-    assert cfg["experiment"]["auto_tuner"]["control"]["max_time_per_task"] == 900
+    assert cfg["experiment"]["auto_tuner"]["control"]["max_time_per_task"] == 7200
+    assert (
+        cfg["experiment"]["exp_dir"]
+        == "${oc.env:FLAGSCALE_OUTPUT_ROOT,./outputs}/qwen3_14b_a800_revalidation/default"
+    )
+    assert 'source "$CONDA_SH"' in cfg["experiment"]["cmds"]["before_start"]
+    assert 'conda activate "$FLAGSCALE_TRAIN_ENV"' in cfg["experiment"]["cmds"]["before_start"]
     assert (
         train_cfg["data"]["data_path"]
-        == "/home/user-a800/GuoChanZhiSuan/data/pile_wikipedia_demo/pile_wikipedia_demo"
+        == "${oc.env:PILE_WIKIPEDIA_DEMO_PATH,../data/pile_wikipedia_demo/pile_wikipedia_demo}"
     )
     assert (
         train_cfg["data"]["tokenizer"]["tokenizer_path"]
-        == "/home/user-a800/GuoChanZhiSuan/models/Qwen3-14B"
+        == "${oc.env:QWEN3_14B_PATH,../models/Qwen3-14B}"
     )
 
 
@@ -50,3 +56,15 @@ def test_qwen3_14b_a800_autotune_keeps_broad_search_space():
     assert space["recompute_num_layers"] == "auto"
     assert train_cfg["model"]["num_attention_heads"] % 4 == 0
     assert train_cfg["model"]["num_query_groups"] % 4 == 0
+
+
+def test_qwen25_configs_use_portable_train_environment_activation():
+    train_cfg = _load_yaml("examples/qwen2_5/conf/train.yaml")
+    autotune_cfg = _load_yaml("examples/qwen2_5/conf/train_auto_tuner.yaml")
+
+    for cfg in (train_cfg, autotune_cfg):
+        before_start = cfg["experiment"]["cmds"]["before_start"]
+        assert 'source "$CONDA_SH"' in before_start
+        assert 'source "$HOME/miniconda3/etc/profile.d/conda.sh"' in before_start
+        assert 'conda activate "$FLAGSCALE_TRAIN_ENV"' in before_start
+        assert "conda activate flagscale-train" in before_start
